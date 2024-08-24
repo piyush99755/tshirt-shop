e0d2c2a6e9a23549a85603ff7fdc36e718b199ed
Piyush Tadvi <piyushtadvi4@gmail.com>
2024-08-24 06:47:47 +0100

    Select product using click event and state, show and hide order summary

--- src/App.js
+++ src/App.js
@@ -1,131 +1,141 @@
 
 import './App.css';
-import Item from './components/item';
+import Item from './components/Item'
 import OrderSummary from './components/OrderSummary';
+import { useState } from 'react';
 
 
 function App() {
   const shopName = 'T-shirt shop made with React JS';
-  const items = [
-    {
+  const [items, setItems] = useState(
+    [
+      {
+          
+          id:1,
+          photo: "img1.webp",
+          name: "Joker",
+          price: 44.99,
+          active: false,
+          quantity: 1, 
+          isInBag: false
+  
+          
+          
+      },
+  
+      {
         
-        id:1,
-        photo: "img1.webp",
-        name: "Joker",
-        price: 44.99,
-        active: false,
-        quantity: 1, 
-        isInBag: false
-
-        
-        
-    },
-
-    {
-      
-        id: 2, 
-        photo: "img2.webp",
-        name: "England",
-        price: 49.99,
-        active: false,
-        quantity: 1, 
-        isInBag: false
-
-      
+          id: 2, 
+          photo: "img2.webp",
+          name: "England",
+          price: 49.99,
+          active: false,
+          quantity: 1, 
+          isInBag: false
   
         
-    },
-    {
-      
-        id: 3, 
-        photo: "img3.webp",
-        name: "2008",
-        price: 39.99,
-        active: false,
-        quantity: 1, 
-        isInBag: false
-
-      
+    
+          
+      },
+      {
         
-    },
-    {
+          id: 3, 
+          photo: "img3.webp",
+          name: "2008",
+          price: 39.99,
+          active: false,
+          quantity: 1, 
+          isInBag: false
+  
         
-          id: 4, 
-         photo: "img4.webp",
-         name: "LNA",
-         price: 29.99,
-         active: false,
-         quantity: 1, 
-         isInBag: false
+          
+      },
+      {
+          
+            id: 4, 
+           photo: "img4.webp",
+           name: "LNA",
+           price: 29.99,
+           active: false,
+           quantity: 1, 
+           isInBag: false
+          
+      },
+      {
         
-    },
-    {
-      
-        id: 5, 
-        photo: "img5.webp",
-        name: "Diablo",
-        price: 59.99,
-        active: false,
-        quantity: 1, 
-        isInBag: false
-
-      
+          id: 5, 
+          photo: "img5.webp",
+          name: "Diablo",
+          price: 59.99,
+          active: false,
+          quantity: 1, 
+          isInBag: false
+  
         
-    },
-    {
-      
-        id: 6, 
-        photo: "img6.webp",
-        name: "Beef",
-        price: 24.99,
-        active: false,
-        quantity: 1, 
-        isInBag: false
-
-      
-       
-    },
-    {
-      
-        id: 7, 
-        photo: "img7.webp",
-        name: "Vince",
-        price: 49.99,
-        active: false,
-        quantity: 1, 
-        isInBag: false
-
-      
+          
+      },
+      {
         
-    },
-    {
-      
-        id: 8, 
-        photo: "img8.webp",
-        name: "Tea",
-        price: 24.99,
-        active: false,
-        quantity: 1, 
-        isInBag: false
-
-      
+          id: 6, 
+          photo: "img6.webp",
+          name: "Beef",
+          price: 24.99,
+          active: false,
+          quantity: 1, 
+          isInBag: false
+  
         
-    },
-    {
-      
-        id: 9, 
-        photo: "img9.webp",
-        name: "London",
-        price: 39.99,
-        active: false,
-        quantity: 1, 
-        isInBag: false
+         
+      },
+      {
+        
+          id: 7, 
+          photo: "img7.webp",
+          name: "Vince",
+          price: 49.99,
+          active: false,
+          quantity: 1, 
+          isInBag: false
+  
+        
+          
+      },
+      {
+        
+          id: 8, 
+          photo: "img8.webp",
+          name: "Tea",
+          price: 24.99,
+          active: false,
+          quantity: 1, 
+          isInBag: false
+  
+        
+          
+      },
+      {
+        
+          id: 9, 
+          photo: "img9.webp",
+          name: "London",
+          price: 39.99,
+          active: false,
+          quantity: 1, 
+          isInBag: false
+  
+        
+         
+      }]
 
-      
-       
-    }];
+  );
 
     let itemsInBag = items.filter(item => item.isInBag);
+
+    function selectHandler(id) {
+     let item =  items.filter(item => item.id === id)[0];
+     item.isInBag = !item.isInBag;
+     setItems(items.map(element => element.id === id ? item : element));
+    }
 
   return(
     <>
@@ -133,13 +143,17 @@ function App() {
             <h1>{shopName}</h1>
             {
               items.map(item => 
-                <Item item={item} />
-             )
-
+                <Item 
+                item = {item}
+                key = {item.id}
+                selectProduct = {id => selectHandler(id)} 
+                />
+              )
             }
             
+            
         </section>
-        <OrderSummary itemsInBag = {itemsInBag}/>
+        {itemsInBag.length > 0 && <OrderSummary />}
           
             
    
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,19 +1,23 @@
-function Item({item}) {
+function Item({item, selectProduct}) {
     return (
         <>
            
-                <div className={`product ${item.isInBag ? 'selected' : ''}`}>
+                <div onClick = {() =>selectProduct(item.id)} className={`product ${item.isInBag ? 'selected' : ''}`}>
                     <div className="photo">
                         <img src={"././img/" + item.photo} alt='' />
                     </div>
                     <div className="description">
                         <span className="name">{item.name}</span>
                         <span className="price">{item.price}</span>
-                        <div className="quantity-area">
+                        {item.isInBag &&
+                            <div className="quantity-area">
                             <button>-</button>
-                            <span className="quantity">1</span>
+                            <span className="quantity">{item.quantity}</span>
                             <button>+</button>
                         </div>
+
+                        }
+                        
                     </div>
                 </div>
 
